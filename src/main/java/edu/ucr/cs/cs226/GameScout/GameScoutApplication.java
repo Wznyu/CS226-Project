d2d9a0e990d585553450feab4190458d4bf56bbd
Stephen Huang <stephenhuang@Stephens-Air.lan>
2024-11-03 18:57:39 -0800

    first commit

---
+++ src/main/java/edu/ucr/cs/cs226/GameScout/GameScoutApplication.java
@@ -0,0 +1,13 @@
+package edu.ucr.cs.cs226.GameScout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameScoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameScoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucr/cs/cs226/GameScout/GameScoutApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucr.cs.cs226.GameScout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameScoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
