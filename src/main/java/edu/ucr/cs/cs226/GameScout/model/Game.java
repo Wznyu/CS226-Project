41b14c0d63bc133cf9ea3a59e3ed4a396658925c
Wesley <wesley@Wesleys-MacBook-Air-4.local>
2024-11-07 15:22:10 -0800

    add images for search games

--- src/main/java/edu/ucr/cs/cs226/GameScout/model/Game.java
+++ src/main/java/edu/ucr/cs/cs226/GameScout/model/Game.java
@@ -1,11 +1,15 @@
 package edu.ucr.cs.cs226.GameScout.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Game {
     private String name;
     private String short_description;
     private String release_date;
     private String number_of_english_reviews;
     private String link;
+
+    private String imgSrc;
 
     public String getName() {
         return name;
@@ -27,6 +31,10 @@ public class Game {
         return release_date;
     }
 
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
     public void setRelease_date(String release_date) {
         this.release_date = release_date;
     }
@@ -39,11 +47,28 @@ public class Game {
         this.number_of_english_reviews = number_of_english_reviews;
     }
 
+    public void setImgSrc() {
+        String link = getLink();
+
+        Pattern pattern = Pattern.compile("/app/(\\d+)/");
+        Matcher matcher = pattern.matcher(link);
+
+        if (matcher.find()) {
+            String id = matcher.group(1);
+            this.imgSrc = String.format(
+                    "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/%s/header.jpg",
+                    id);
+        } else {
+            this.imgSrc = "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png";
+        }
+    }
+
     public String getLink() {
         return link;
     }
 
     public void setLink(String link) {
         this.link = link;
+        setImgSrc();
     }
 }
